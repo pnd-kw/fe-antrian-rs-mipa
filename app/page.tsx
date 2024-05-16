832562aa3d7c9f013f51a68420314e988ecb5190
Pandu Office PC <pandu.usahaku@gmail.com>
2024-05-16 10:29:58 +0700

    style: modify layout and add images

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,53 @@ import PusherConnection from "@/components/PusherConnection";
 export default function Home() {
   return (
     <div className="relative h-screen">
-      <PusherConnection/>
-      <div className="w-full h-28" style={{ backgroundColor: "#009b4c" }}>
+      <PusherConnection />
+      <div
+        className="w-full h-28"
+        style={{
+          background: "linear-gradient(to right, #009b4c, #00587a, #00c8d7)",
+        }}
+      >
         <Grid container spacing={2}>
           <Grid item xs={12} md={6}>
-            <div className="ml-8 mt-6">
-              <div className="font-24 font-bold uppercase text-white">
-                antrian pasien rsu mitra paramedika
+            <div className="flex justify-start">
+              <div className="ml-4 mt-3">
+                <Image
+                  src="/images/logo-rsu-mipa.png"
+                  alt="Logo RSU Mipa"
+                  width={70}
+                  height={70}
+                />
               </div>
-            </div>
-            <div className="ml-8 mt-2">
-              <div className="font-14 text-white">Senin, 13 Mei 2024</div>
+              <div className="ml-6 mt-6">
+                <div className="font-24 font-bold uppercase text-white">
+                  antrian pasien rsu mitra paramedika
+                </div>
+                <div className="font-14 text-white">Senin, 13 Mei 2024</div>
+              </div>
             </div>
           </Grid>
           <Grid item xs={12} md={6}>
-            <div className="flex justify-end">
-              <div className="font-24"></div>
+            <div
+              className="absolute top-0 right-0 h-28 w-full"
+              style={{
+                opacity: 0.2,
+                backgroundImage: "url('images/rsu-mipa.jpg')",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+              }}
+            ></div>
+            <div className="flex justify-end items-center h-full pr-8">
+              <div className="font-24">
+                <Image
+                  src="/images/logo-kars.png"
+                  alt="Logo Kars"
+                  width={100}
+                  height={300}
+                  className="z-10"
+                />
+              </div>
             </div>
           </Grid>
         </Grid>
@@ -28,8 +59,13 @@ export default function Home() {
       <div className="w-full">
         <Grid container spacing={2}>
           <Grid item xs={12} md={6} className="relative">
-            <div className="absolute w-2/5 h-44 right-4 top-4 mt-2 bg-white p-2 border-2 rounded-xl border-green-400">
-              <Grid container spacing={1}>
+            <div className="absolute w-2/5 h-56 right-4 top-4 mt-2 bg-white p-2 border-2 rounded-xl border-green-400">
+              <div className="flex justify-center">
+                <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-green-700">
+                  no. antrian
+                </div>
+              </div>
+              {/* <Grid container spacing={1}>
                 <Grid item xs={12} md={6}>
                   <div className="flex justify-center">
                     <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-orange-400">
@@ -44,8 +80,13 @@ export default function Home() {
                     </div>
                   </div>
                 </Grid>
-              </Grid>
-              <div className="h-32 flex items-center">
+              </Grid> */}
+              <div className="h-44 flex justify-center items-center">
+                <div className="font-bold text-4xl md:text-6xl lg:text-8xl text-green-600">
+                  4
+                </div>
+              </div>
+              {/* <div className="h-44 flex items-center">
                 <Grid container spacing={1}>
                   <Grid item xs={12} md={6}>
                     <div className="flex justify-center">
@@ -60,13 +101,41 @@ export default function Home() {
                     </div>
                   </Grid>
                 </Grid>
-              </div>
+              </div> */}
             </div>
-            <div className="mx-4 my-4 h-40 border-2 rounded-xl border-green-400"></div>
+            <div className="mx-4 my-4 h-52 flex items-center border-2 rounded-xl border-green-400">
+              <Grid container spacing={1}>
+                <Grid item xs={12} md={2.5}>
+                  <Image
+                    src="/images/dr_layung.jpg"
+                    alt="drg Layung sekar prabarayi"
+                    width={200}
+                    height={500}
+                  />
+                </Grid>
+                <Grid item xs={12} md={4.5}>
+                  <div className="flex justify-center mb-12">
+                    <div className="font-bold text-lg md:text-xl lg:text-2xl uppercase">
+                      POLI GIGI
+                    </div>
+                  </div>
+                  <div className="flex justify-center mb-2">
+                    <div className="font-semibold text-xs md:text-xs lg:text-sm">
+                      drg. Layung Sekar Prabarayi
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
           </Grid>
           <Grid item xs={12} md={6} className="relative">
-            <div className="absolute w-2/5 h-44 right-4 top-4 mt-2 bg-white p-2 border-2 rounded-xl border-green-400">
-              <Grid container spacing={1}>
+            <div className="absolute w-2/5 h-56 right-4 top-4 mt-2 bg-white p-2 border-2 rounded-xl border-green-400">
+              <div className="flex justify-center">
+                <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-green-700">
+                  no. antrian
+                </div>
+              </div>
+              {/* <Grid container spacing={1}>
                 <Grid item xs={12} md={6}>
                   <div className="flex justify-center">
                     <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-orange-400">
@@ -81,8 +150,13 @@ export default function Home() {
                     </div>
                   </div>
                 </Grid>
-              </Grid>
-              <div className="h-32 flex items-center">
+              </Grid> */}
+              <div className="h-44 flex justify-center items-center">
+                <div className="font-bold text-4xl md:text-6xl lg:text-8xl text-green-600">
+                  1325
+                </div>
+              </div>
+              {/* <div className="h-44 flex items-center">
                 <Grid container spacing={1}>
                   <Grid item xs={12} md={6}>
                     <div className="flex justify-center">
@@ -97,9 +171,175 @@ export default function Home() {
                     </div>
                   </Grid>
                 </Grid>
-              </div>
+              </div> */}
             </div>
-            <div className="mx-4 my-4 h-40 border-2 rounded-xl border-green-400"></div>
+            <div className="mx-4 my-4 h-52 flex items-center border-2 rounded-xl border-green-400">
+              <Grid container spacing={1}>
+                <Grid item xs={12} md={2.5}>
+                  <Image
+                    src="/images/dr_febriza.png"
+                    alt="drg Layung sekar prabarayi"
+                    width={200}
+                    height={500}
+                    objectFit="cover"
+                  />
+                </Grid>
+                <Grid item xs={12} md={4.5}>
+                  <div className="flex justify-center mb-12">
+                    <div className="font-bold text-lg md:text-xl lg:text-2xl uppercase">
+                      POLI GIGI
+                    </div>
+                  </div>
+                  <div className="flex justify-center mb-2">
+                    <div className="font-semibold text-xs md:text-xs lg:text-sm">
+                      drg. Febriza
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6} className="relative">
+            <div className="absolute w-2/5 h-56 right-4 top-4 mt-8 bg-white p-2 border-2 rounded-xl border-green-400">
+              <div className="flex justify-center">
+                <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-green-700">
+                  no. antrian
+                </div>
+              </div>
+              {/* <Grid container spacing={1}>
+                <Grid item xs={12} md={6}>
+                  <div className="flex justify-center">
+                    <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-orange-400">
+                      ulang
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <div className="flex justify-center">
+                    <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-green-700">
+                      terakhir
+                    </div>
+                  </div>
+                </Grid>
+              </Grid> */}
+              <div className="h-44 flex justify-center items-center">
+                <div className="font-bold text-4xl md:text-6xl lg:text-8xl text-green-600">
+                  4
+                </div>
+              </div>
+              {/* <div className="h-44 flex items-center">
+                <Grid container spacing={1}>
+                  <Grid item xs={12} md={6}>
+                    <div className="flex justify-center">
+                      <div className="font-bold text-xl md:text-3xl lg:text-6xl text-green-600"></div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <div className="flex justify-center">
+                      <div className="font-bold text-xl md:text-3xl lg:text-6xl text-green-600">
+                        4
+                      </div>
+                    </div>
+                  </Grid>
+                </Grid>
+              </div> */}
+            </div>
+            <div className="mx-4 mt-10 h-52 flex items-center border-2 rounded-xl border-green-400">
+              <Grid container spacing={1}>
+                <Grid item xs={12} md={2.5}>
+                  <Image
+                    src="/images/dr_fajar.png"
+                    alt="dr Fajar"
+                    width={90}
+                    height={200}
+                  />
+                </Grid>
+                <Grid item xs={12} md={4.5}>
+                  <div className="flex justify-center mb-12">
+                    <div className="font-bold text-lg md:text-xl lg:text-2xl uppercase">
+                      POLI
+                    </div>
+                  </div>
+                  <div className="flex justify-center mb-2">
+                    <div className="font-semibold text-xs md:text-xs lg:text-sm">
+                      dr. Fajar
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
+          </Grid>
+          <Grid item xs={12} md={6} className="relative">
+            <div className="absolute w-2/5 h-56 right-4 top-4 mt-8 bg-white p-2 border-2 rounded-xl border-green-400">
+              <div className="flex justify-center">
+                <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-green-700">
+                  no. antrian
+                </div>
+              </div>
+              {/* <Grid container spacing={1}>
+                <Grid item xs={12} md={6}>
+                  <div className="flex justify-center">
+                    <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-orange-400">
+                      ulang
+                    </div>
+                  </div>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <div className="flex justify-center">
+                    <div className="text-sm md:text-base lg:text-lg font-bold uppercase text-green-700">
+                      terakhir
+                    </div>
+                  </div>
+                </Grid>
+              </Grid> */}
+              <div className="h-44 flex justify-center items-center">
+                <div className="font-bold text-4xl md:text-6xl lg:text-8xl text-green-600">
+                  1325
+                </div>
+              </div>
+              {/* <div className="h-44 flex items-center">
+                <Grid container spacing={1}>
+                  <Grid item xs={12} md={6}>
+                    <div className="flex justify-center">
+                      <div className="font-bold text-xl md:text-3xl lg:text-6xl text-green-600"></div>
+                    </div>
+                  </Grid>
+                  <Grid item xs={12} md={6}>
+                    <div className="flex justify-center">
+                      <div className="font-bold text-xl md:text-3xl lg:text-6xl text-green-600">
+                        132
+                      </div>
+                    </div>
+                  </Grid>
+                </Grid>
+              </div> */}
+            </div>
+            <div className="mx-4 mt-10 h-52 flex items-center border-2 rounded-xl border-green-400">
+            <Grid container spacing={1}>
+                <Grid item xs={12} md={2.5}>
+                  <Image
+                    src="/images/dr_taufik.jpg"
+                    alt="dr Taufik R Alkaff SpOg"
+                    width={200}
+                    height={500}
+                  />
+                </Grid>
+                <Grid item xs={12} md={4.5}>
+                  <div className="flex justify-center mb-12">
+                    <div className="font-bold text-lg md:text-xl lg:text-2xl uppercase">
+                      POLI OBSGYN
+                    </div>
+                  </div>
+                  <div className="flex justify-center mb-2">
+                    <div className="font-semibold text-xs md:text-xs lg:text-sm">
+                      dr. Taufik R Alkaff SpOG
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </div>
           </Grid>
         </Grid>
       </div>
